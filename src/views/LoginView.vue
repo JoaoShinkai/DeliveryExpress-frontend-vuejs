5dbafd7f1ccfd5929a2159bcbe708d4f0e18f59c
unknown <joaogabriel1412@gmail.com>
2022-06-03 12:15:54 -0300

    First commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login">
+        <v-card elevation="2" class="login-card">
+            <v-card-title>Login</v-card-title>
+            <v-card-text>
+                <v-text-field label="Email" outlined></v-text-field>
+                <v-text-field label="Senha" outlined></v-text-field>
+                <v-btn depressed color="primary">
+                    Entrar
+                </v-btn>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.login{
+    background-color: #dfe4ea;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-card{
+    width: clamp(350px, 50%, 700px);
+}
+</style>
