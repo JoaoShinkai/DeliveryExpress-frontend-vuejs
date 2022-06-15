0f49133191eef18c396de10dde56675bb7e080c7
unknown <joaogabriel1412@gmail.com>
2022-06-15 11:43:55 -0300

    Create option additional and delete additionals ok

--- src/views/store/StoreEditProduct.vue
+++ src/views/store/StoreEditProduct.vue
@@ -21,7 +21,7 @@
                         </v-row>
                         <v-text-field v-model="product.description" label="Descrição" outlined></v-text-field>
                         <div class="store-edit-product-container-info-form-divBtn">
-                            <v-btn type="submit" color="error">Editar</v-btn>
+                            <v-btn type="submit" color="#11101d" style="color: white">Enviar</v-btn>
                         </div>
                     </form>
                 </v-card-text>
@@ -34,7 +34,7 @@
                         <form @submit.prevent="createAdditional()">
                             <v-text-field v-model="createAdditionalProps.name" label="Novo adicional" outlined></v-text-field>
                             <div class="card-adicionais-novo-divBtn">
-                                <v-btn type="submit" color="error">Adicionar</v-btn>
+                                <v-btn type="submit"  color="#11101d" style="color: white">Adicionar</v-btn>
                             </div>
                         </form>
                         
@@ -44,7 +44,9 @@
                     <v-card-title>
                         <div class="card-adicionais-item-title">
                             <div>{{additional.name}}</div>
-                            <div><i class="fa-solid fa-trash"></i></div>
+                            <v-btn @click="deleteAdditional(additional)" fab dark color="error" small style="box-shadow: none; font-size: 16px">
+                                <i class="fa-solid fa-trash"></i>
+                            </v-btn>
                         </div>
                     </v-card-title>
                     
@@ -55,15 +57,20 @@
                             <div class="card-adicionais-item-option-price">{{optionAdditional.price}}</div>
                         </div>
                         <div class="card-adicionais-item-newOption">
-                            <v-row>
+                            
+                            <form :id="additional.id" @submit.prevent="createOptionAdditional(additional.id)">
+                                <v-row>
                                     <v-col cols="12" sm="9">
-                                        <v-text-field v-model="createOptionAdditionalProps.name" label="Outlined" outlined dense></v-text-field>
+                                        <v-text-field ref="inputRef" clearable label="Adicional" outlined dense required></v-text-field>
                                     </v-col>
                                     <v-col cols="12" sm="3">
-                                        <v-text-field label="Outlined" outlined dense></v-text-field>
+                                        <v-text-field ref="inputRef" clearable type="number" step="0.01" label="Valor adicional" prefix="R$" outlined dense required></v-text-field>
                                     </v-col>
-                            </v-row>
-                            
+                                </v-row>
+                                <div class="card-adicionais-novo-divBtn">
+                                    <v-btn color="error" style="color: white" type="submit">Adicionar</v-btn>
+                                </div>
+                            </form>
                         </div>
                         
                     </v-card-text>
@@ -75,13 +82,25 @@
         <!-- Alerta de sucesso -->
         <v-snackbar v-model="snackbar" color="green" style="justify-content: end; align-items: flex-start">
             Atualizado com sucesso
-
             <template v-slot:action="{ attrs }">
                 <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
                 Fechar
                 </v-btn>
             </template>
         </v-snackbar>
+
+        <!-- Modal de exclusão de adicional -->
+        <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+                <v-card-title class="body-1">Deseja realmente deletar o adicional <b style="margin-left: 5px"> {{modalDeleteAdditional.name}}</b>?</v-card-title>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialogDelete = false" >Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteAdditionalConfirm()" >OK</v-btn>
+                <v-spacer></v-spacer>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
     
 </template>
@@ -94,6 +113,7 @@ import axios from 'axios'
 export default {
     data() {
         return{
+            dialogDelete: false,
             snackbar: false,
             token: localStorage.getItem('token'),
             items: [
@@ -114,6 +134,10 @@ export default {
             createOptionAdditionalProps: {
                 name: '',
                 price: ''
+            },
+            modalDeleteAdditional: {
+                id: '',
+                name: ''
             },
             additionals: []
         }
@@ -183,6 +207,42 @@ export default {
                 await axios.post(`http://localhost:3000/additional/`, data);
                 this.loadAdditionals();
                 this.cleanCreateAdditionalFields();
+            }catch(err){
+                console.log(err);
+            }
+        },
+
+        async createOptionAdditional(id){
+            const name = document.getElementById(id).getElementsByTagName('input')[0].value;
+            const price = document.getElementById(id).getElementsByTagName('input')[1].value;
+
+            const data = {
+                name,
+                price,
+                additionalId: id
+            }
+
+            try{
+                await axios.post('http://localhost:3000/optionAdditional', data);
+                this.loadAdditionals();
+
+                document.getElementById(id).getElementsByClassName('v-input__append-inner')[1].getElementsByTagName('button')[0].click();
+                document.getElementById(id).getElementsByClassName('v-input__append-inner')[0].getElementsByTagName('button')[0].click();
+            }catch(err){
+                console.log(err);
+            }
+        },
+
+        deleteAdditional(additional){
+            this.modalDeleteAdditional = additional;
+            this.dialogDelete = true;
+        },
+
+        async deleteAdditionalConfirm(){
+            try{
+                await axios.delete(`http://localhost:3000/additional/${this.modalDeleteAdditional.id}`);
+                this.loadAdditionals();
+                this.dialogDelete = false;
             }catch(err){
                 console.log(err);
             }
